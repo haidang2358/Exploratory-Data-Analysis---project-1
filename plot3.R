67d83bdc122261cc7773585c461fa3ca4bd407df
haidang2358 <50066481+haidang2358@users.noreply.github.com>
2019-04-28 17:55:55 +0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -9,5 +9,5 @@ png("plot3.png", width = 480, height = 480)
 plot(datetime, sub1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, sub2, type="l", col="red")
 lines(datetime, sub3, type="l", col = "blue")
-legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("Purple","Red","Blue"))> 
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = 1, lwd = 2.5, col = c("Black","Red","Blue"))
 dev.off
