cba44dcf1ba16babe122f58df54b52bcfd2a1351
haidang2358 <50066481+haidang2358@users.noreply.github.com>
2019-04-28 17:55:04 +0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,6 +3,6 @@ data <- read.table(datafile, header = TRUE, sep = ";", stringsAsFactors = FALSE,
 subsetData <- data[data$Date %in% c("01/02/2007","02/02/2017"),]
 glob <- as.numeric(subsetData$Global_active_power)
 datetime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-png("plot.png", width = 480, height = 480)
+png("plot2.png", width = 480, height = 480)
 plot(datetime, glob, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off
