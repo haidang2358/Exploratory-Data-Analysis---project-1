b99951baf734e5df97d5af27ff34bcb41a4ad584
haidang2358 <50066481+haidang2358@users.noreply.github.com>
2019-04-28 17:16:57 +0700

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,13 @@
+datafile <- ""household_power_consumption.txt")
+data <- read.table(datafile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
+subsetData <- data[data$Date %in% c("01/02/2007","02/02/2017"),]
+glob <- as.numeric(subsetData$Global_active_power)
+sub1<-as.numeric(subsetData$Sub_metering_1)
+sub2<-as.numeric(subsetData$Sub_metering_2)
+sub3<-as.numeric(subsetData$Sub_metering_3)
+png("plot3.png", width = 480, height = 480)
+plot(datetime, sub1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, sub2, type="l", col="red")
+lines(datetime, sub3, type="l", col = "blue")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("Purple","Red","Blue"))> 
+dev.off
