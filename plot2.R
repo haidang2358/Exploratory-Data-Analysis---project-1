29c2c32b3f8cdbf800b33eda212659d36a152f86
haidang2358 <50066481+haidang2358@users.noreply.github.com>
2019-04-28 19:15:38 +0700

    Delete plot2.R

--- plot2.R
+++
@@ -1,8 +0,0 @@
-datafile <- ""household_power_consumption.txt")
-data <- read.table(datafile, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
-subsetData <- data[data$Date %in% c("01/02/2007","02/02/2017"),]
-glob <- as.numeric(subsetData$Global_active_power)
-datetime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-png("plot2.png", width = 480, height = 480)
-plot(datetime, glob, type="l", xlab="", ylab="Global Active Power (kilowatts)")
-dev.off
